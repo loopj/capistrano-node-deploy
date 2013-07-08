186c4ca4ccc423c9211af7ef5fd8af6c7f99c7fa
Ronald Chaplin <rchaplin@t73.biz>
2013-07-08 15:02:06 -0700

    Adjusted to reflect node_env logic

--- lib/capistrano/node-deploy.rb
+++ lib/capistrano/node-deploy.rb
@@ -65,7 +65,7 @@ EOD
       run "mkdir -p #{shared_path}/node_modules"
       run "cp #{release_path}/package.json #{shared_path}"
       run "cp #{release_path}/npm-shrinkwrap.json #{shared_path}"
-      run "cd #{shared_path} && npm install #{(node_env == 'production') ? '--dev' : ''} --loglevel warn"
+      run "cd #{shared_path} && npm install #{(node_env != 'production') ? '--dev' : ''} --loglevel warn"
       run "ln -s #{shared_path}/node_modules #{release_path}/node_modules"
     end
 
